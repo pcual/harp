52a228fa9031b0ba7676e6f33a318dff71198d68
Yash Parghi <yash@yashparghi.com>
2013-12-03 22:00:19 -0500

    Beginnings of resource factory registration.

--- src/main/java/harp/HarpBinding.java
+++ src/main/java/harp/HarpBinding.java
@@ -16,7 +16,10 @@
 
 package harp;
 
+import com.google.common.collect.ImmutableSet;
 import groovy.lang.Binding;
+import harp.resource.ResourceRegistrar;
+import java.util.Set;
 
 /**
  * TODO -- Document that this class isn't just for holding variables. It also stores the base
@@ -24,13 +27,42 @@ import groovy.lang.Binding;
  */
 final class HarpBinding extends Binding {
 
+  /**
+   * Variables or properties you can't change in a Harp script.
+   */
+  private static final Set<String> FORBIDDEN_VARIABLES = ImmutableSet.of(
+      "resource"
+  );
+
   private final ContextBuilder contextToBuild;
+  private final ResourceRegistrar resourceRegistrar;
 
   HarpBinding() {
     contextToBuild = Context.builder();
+    resourceRegistrar = new ResourceRegistrar();
+    super.setVariable("resource", resourceRegistrar);
   }
 
   ContextBuilder getContextBuilder() {
     return contextToBuild;
+  }
+
+  private void checkUnreserved(String variableName) {
+    if (FORBIDDEN_VARIABLES.contains(variableName)) {
+      throw new UnsupportedOperationException(
+          "The property '" + variableName + "' is reserved in a Harp script.");
+    }
+  }
+
+  @Override
+  public void setProperty(String property, Object newValue) {
+    checkUnreserved(property);
+    super.setProperty(property, newValue);
+  }
+
+  @Override
+  public void setVariable(String name, Object newValue) {
+    checkUnreserved(name);
+    super.setVariable(name, newValue);
   }
 }
--- src/main/java/harp/resource/ResourceRegistrar.java
+++ src/main/java/harp/resource/ResourceRegistrar.java
@@ -16,11 +16,43 @@
 
 package harp.resource;
 
+import com.google.common.base.Preconditions;
+import groovy.lang.Closure;
 import groovy.lang.GroovyObjectSupport;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * TODO
  */
+// TODO Test resource declaration with the registrar in HarpScriptTest.
 public final class ResourceRegistrar extends GroovyObjectSupport {
 
+  private final Map<String, ResourceFactory> resourceFactories = new HashMap<>();
+
+  @Override
+  public Object invokeMethod(String name, Object args) {
+    if (!(args instanceof Object[])) {
+      throw new IllegalArgumentException(
+          "The ResourceRegistrar expected an array of args but didn't get one.");
+    }
+    Object[] arrayArgs = (Object[]) args;
+    if (arrayArgs.length != 1 || !(arrayArgs[0] instanceof Closure)) {
+      throw new IllegalArgumentException(
+          "Resources must be declared with one argument: a closure.");
+    }
+
+    // TODO delegate to the right ResourceFactory and add it to this script's ContextBuilder.
+    return null;
+  }
+
+  /**
+   * TODO
+   */
+  public final void registerResourceFactory(String name, ResourceFactory factory) {
+    Preconditions.checkArgument(
+        !resourceFactories.containsKey(name),
+        "You've already registered a resource factory named '" + name + "'!");
+    resourceFactories.put(name, factory);
+  }
 }
