e549bcfde19ca19ef881ee1ec7b09a296a073cf6
Yash Parghi <yash@yashparghi.com>
2013-12-15 16:01:08 -0500

    Remove an obsolete comment

--- src/main/java/harp/executable/ExecutableBuilder.java
+++ src/main/java/harp/executable/ExecutableBuilder.java
@@ -42,7 +42,6 @@ public class ExecutableBuilder {
     this.args = ImmutableList.copyOf(args);
   }
 
-    // TODO!!! RENAME THIS BACK TO RESOURCE AND TEST
   public void resources(String... resources) {
     Preconditions.checkState(this.resources == null);
     Preconditions.checkNotNull(resources);
